42955c2999272aae78df08f882ef1c524b1a0151
Yusuke Gunji <inochideutae@gmail.com>
2020-04-16 22:40:56 +0900

    feat:welcome-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeModule } from './welcome/welcome.module';
 
-
 const routes: Routes = [
   {
     path: 'welcome',

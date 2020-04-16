8f5931a9c9786ffae44d0297f6f7e2aee48a82c2
Yusuke Gunji <inochideutae@gmail.com>
2020-04-16 14:14:18 +0900

    feat: add welcom.comp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomModule } from './welcom/welcom.module';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'welcom',
+    loadChildren: () => import('./welcome/welcome.module').then(m => m.WelcomeModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

01aa3c5a1cc6988e57fb23f211f7f4b0265016f2
Yusuke Gunji <inochideutae@gmail.com>
2020-04-17 15:45:45 +0900

    feat(create):ペット選択カルーセル追加

--- src/app/create/create.module.ts
+++ src/app/create/create.module.ts
@@ -8,6 +8,7 @@ import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatButtonModule} from '@angular/material/button';
 import {MatRadioModule} from '@angular/material/radio';
+import { SwiperModule } from 'ngx-swiper-wrapper';
 
 @NgModule({
   declarations: [CreateComponent],
@@ -19,7 +20,8 @@ import {MatRadioModule} from '@angular/material/radio';
     MatInputModule,
     MatFormFieldModule,
     MatButtonModule,
-    MatRadioModule
+    MatRadioModule,
+    SwiperModule
   ]
 })
 export class CreateModule { }
--- src/app/create/create/create.component.ts
+++ src/app/create/create/create.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators, FormControl } from '@angular/forms';
+import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
 
 @Component({
   selector: 'app-create',
@@ -7,6 +8,16 @@ import { FormBuilder, Validators, FormControl } from '@angular/forms';
   styleUrls: ['./create.component.scss']
 })
 export class CreateComponent implements OnInit {
+
+  petIds = [...Array(10)].map((_, i) => i + 1);
+  config: SwiperConfigInterface = {
+    loop: true,
+    navigation: true,
+    pagination: true,
+    centeredSlides: true,
+    slidesPerView: 3
+  };
+  selectedPetId = 0;
 
   form = this.fb.group({
     name: ['', [
@@ -32,6 +43,7 @@ export class CreateComponent implements OnInit {
 
   submit() {
     console.log(this.form.value);
+    console.log(this.selectedPetId);
   }
 
 }
